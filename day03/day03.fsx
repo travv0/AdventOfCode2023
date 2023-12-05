681bd344ea030a0b781c7d363923d0863d680f2e
Travis <tssund93@gmail.com>
2023-12-05 12:01:21 -0600

    day 3 part 2

--- day03/day03.fsx
+++ day03/day03.fsx
@@ -1,28 +1,64 @@
 ﻿open System
 open System.IO
 
-let input =
-    File.ReadAllLines("input.txt")
-    |> Array.map (fun line -> line + ".")
+type Symbol = { symbol: char; x: int; y: int }
+
+let input = File.ReadAllLines("input.txt") |> Array.map (fun line -> line + ".")
 
 let width = String.length input[0]
 let height = Array.length input
 
-let isBySymbol x y (input: string[]) =
+let getAdjacentSymbol x y (input: string[]) : Symbol option =
     let isSymbol c = c |> Char.IsDigit |> not && c <> '.'
-    y > 0 && isSymbol input.[y - 1].[x]
-    || y < height - 1 && isSymbol input.[y + 1].[x]
-    || x > 0 && isSymbol input.[y].[x - 1]
-    || x < width - 1 && isSymbol input.[y].[x + 1]
-    || y > 0 && x > 0 && isSymbol input.[y - 1].[x - 1]
-    || y < height - 1 && x > 0 && isSymbol input.[y + 1].[x - 1]
-    || y > 0 && x < width - 1 && isSymbol input.[y - 1].[x + 1]
-    || y < height - 1 && x < width - 1 && isSymbol input.[y + 1].[x + 1]
 
-let mutable partNumbers = []
+    if y > 0 && isSymbol input.[y - 1].[x] then
+        Some
+            { symbol = input.[y - 1].[x]
+              x = x
+              y = y - 1 }
+    elif y < height - 1 && isSymbol input.[y + 1].[x] then
+        Some
+            { symbol = input.[y + 1].[x]
+              x = x
+              y = y + 1 }
+    elif x > 0 && isSymbol input.[y].[x - 1] then
+        Some
+            { symbol = input.[y].[x - 1]
+              x = x - 1
+              y = y }
+    elif x < width - 1 && isSymbol input.[y].[x + 1] then
+        Some
+            { symbol = input.[y].[x + 1]
+              x = x + 1
+              y = y }
+    elif y > 0 && x > 0 && isSymbol input.[y - 1].[x - 1] then
+        Some
+            { symbol = input.[y - 1].[x - 1]
+              x = x - 1
+              y = y - 1 }
+    elif y < height - 1 && x > 0 && isSymbol input.[y + 1].[x - 1] then
+        Some
+            { symbol = input.[y + 1].[x - 1]
+              x = x - 1
+              y = y + 1 }
+    elif y > 0 && x < width - 1 && isSymbol input.[y - 1].[x + 1] then
+        Some
+            { symbol = input.[y - 1].[x + 1]
+              x = x + 1
+              y = y - 1 }
+    elif y < height - 1 && x < width - 1 && isSymbol input.[y + 1].[x + 1] then
+        Some
+            { symbol = input.[y + 1].[x + 1]
+              x = x + 1
+              y = y + 1 }
+    else
+        None
+
+let mutable partNumbers = Map.empty
+
 for y in 0 .. height - 1 do
     let mutable number = ""
-    let mutable bySymbol = false
+    let mutable adjacentSymbol = None
 
     for x in 0 .. width - 1 do
         let c = input.[y].[x]
@@ -30,13 +66,32 @@ for y in 0 .. height - 1 do
         if Char.IsDigit c then
             number <- number + string c
 
-            if isBySymbol x y input then
-                bySymbol <- true
+            match getAdjacentSymbol x y input with
+            | Some symbol -> adjacentSymbol <- Some symbol
+            | None -> ()
         elif number <> "" then
-            if bySymbol then
-                partNumbers <- int number :: partNumbers
+            match adjacentSymbol with
+            | Some symbol ->
+                partNumbers <-
+                    Map.change
+                        symbol
+                        (function
+                        | Some pns -> Some(int number :: pns)
+                        | None -> Some [ int number ])
+                        partNumbers
+            | None -> ()
 
             number <- ""
-            bySymbol <- false
+            adjacentSymbol <- None
 
-partNumbers |> List.sum |> printfn "Part 1: %d"
+partNumbers |> Map.values |> Seq.concat |> Seq.sum |> printfn "Part 1: %d"
+
+partNumbers
+|> Map.keys
+|> Seq.filter (fun { symbol = symbol } -> symbol = '*')
+|> Seq.map (fun symbol ->
+    match Map.find symbol partNumbers with
+    | [ num1; num2 ] -> num1 * num2
+    | _ -> 0)
+|> Seq.sum
+|> printfn "Part 2: %d"
