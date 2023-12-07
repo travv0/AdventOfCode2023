7ea54b31e4f0301f9544fd368334d2e00543e382
Travis <tssund93@gmail.com>
2023-12-07 17:00:54 -0600

    day 7 part 2

--- day07/day07.fsx
+++ day07/day07.fsx
@@ -3,75 +3,103 @@ open System
 type Hand = { cards: string; bid: int }
 
 type HandTypes =
-    | FiveOfAKind
-    | FourOfAKind
-    | FullHouse
-    | ThreeOfAKind
-    | TwoPair
-    | OnePair
     | HighCard
+    | OnePair
+    | TwoPair
+    | ThreeOfAKind
+    | FullHouse
+    | FourOfAKind
+    | FiveOfAKind
 
 let parseInput (lines: string seq) =
     [ for line in lines do
           let parts = line.Split(' ')
+          { cards = parts[0]; bid = int parts[1] } ]
 
-          { cards = parts.[0]
-            bid = int parts.[1] } ]
+let replaceJokers (hand: string) : string list =
+    let cards = hand |> Seq.filter ((<>) 'J') |> Seq.distinct
+
+    let replacements =
+        hand
+        |> Seq.map (function
+            | 'J' -> cards
+            | other -> [ other ])
+
+    match
+        replacements
+        |> Seq.fold
+            (fun hands cardOptions ->
+                [ for hand in hands do
+                      for card in cardOptions do
+                          yield hand + (string card) ])
+            [ "" ]
+    with
+    | [] -> [ "JJJJJ" ]
+    | r -> r
 
 let classifyHand hand =
-    let groups = Seq.groupBy (fun c -> c) hand |> Seq.map snd
+    let numRank n r = Seq.exists (Seq.length >> (=) n) r
 
-    if Seq.exists (fun g -> Seq.length g = 5) groups then
-        FiveOfAKind
-    elif Seq.exists (fun g -> Seq.length g = 4) groups then
-        FourOfAKind
-    elif
-        Seq.exists (fun g -> Seq.length g = 3) groups
-        && Seq.exists (fun g -> Seq.length g = 2) groups
-    then
-        FullHouse
-    elif Seq.exists (fun g -> Seq.length g = 3) groups then
-        ThreeOfAKind
-    elif Seq.filter (fun g -> Seq.length g = 2) groups |> Seq.length = 2 then
-        TwoPair
-    elif Seq.exists (fun g -> Seq.length g = 2) groups then
-        OnePair
-    else
+    let countRanks n r =
+        Seq.filter (Seq.length >> (=) n) r |> Seq.length
+
+    let ranks = hand |> Seq.groupBy id |> Seq.map snd |> List.ofSeq
+
+    match () with
+    | _ when numRank 5 ranks -> FiveOfAKind
+    | _ when numRank 4 ranks -> FourOfAKind
+    | _ when numRank 3 ranks && numRank 2 ranks -> FullHouse
+    | _ when numRank 3 ranks -> ThreeOfAKind
+    | _ when countRanks 2 ranks = 2 -> TwoPair
+    | _ when numRank 2 ranks -> OnePair
+    | _ -> HighCard
+
+let classifyHandWithJokers (hand: string) : HandTypes =
+    replaceJokers hand
+    |> Seq.fold
+        (fun bestHand hand ->
+            let handType = classifyHand hand
+            if handType > bestHand then handType else bestHand)
         HighCard
 
-let cardComparer (card1: char) (card2: char) : int =
+let cardComparer jValue (card1: char) (card2: char) : int =
     let cardToValue =
         function
         | 'A' -> 14
         | 'K' -> 13
         | 'Q' -> 12
-        | 'J' -> 11
+        | 'J' -> jValue
         | 'T' -> 10
         | n -> int $"%c{n}"
 
-    let value1 = cardToValue card1
-    let value2 = cardToValue card2
-    value1 - value2
+    cardToValue card1 - cardToValue card2
 
-let handComparer (hand1: Hand) (hand2: Hand) : int =
-    let hand1Type = classifyHand hand1.cards
-    let hand2Type = classifyHand hand2.cards
+let handComparer classifyFn jValue (hand1: Hand) (hand2: Hand) : int =
+    let hand1Type = classifyFn hand1.cards
+    let hand2Type = classifyFn hand2.cards
 
-    if hand1Type > hand2Type then
+    if hand1Type < hand2Type then
         -1
-    elif hand2Type > hand1Type then
+    elif hand1Type > hand2Type then
         1
     else
-        Seq.zip hand1.cards hand2.cards
-        |> Seq.find (fun (c1, c2) -> c1 <> c2)
-        ||> cardComparer
+        match Seq.zip hand1.cards hand2.cards |> Seq.tryFind (fun (c1, c2) -> c1 <> c2) with
+        | Some cards -> cards ||> cardComparer jValue
+        | None -> 0
 
 let input = IO.File.ReadAllLines("input.txt")
 let hands = parseInput input
 
 hands
-|> Seq.sortWith handComparer
+|> Seq.sortWith (handComparer classifyHand 11)
 |> Seq.zip [ 1 .. List.length hands ]
 |> Seq.map (fun (rank, hand) -> rank * hand.bid)
 |> Seq.sum
 |> printfn "Part 1: %d"
+
+hands
+|> Seq.sortWith (handComparer classifyHandWithJokers 1)
+|> Seq.zip [ 1 .. List.length hands ]
+|> Seq.map (fun (rank, hand) -> rank * hand.bid)
+|> Seq.sum
+|> printfn "Part 2: %d"
