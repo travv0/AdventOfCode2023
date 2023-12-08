b03dd2f81efeee544ee39cb65c070f303258ce70
Travis <tssund93@gmail.com>
2023-12-08 17:27:41 -0600

    slow day 8 part 2

--- day08/day08.fsx
+++ day08/day08.fsx
@@ -15,8 +15,8 @@ let parseNode (s: string) =
     let children = parts[1].Split(", ")
 
     { label = parts[0]
-      left = String.filter Char.IsLetter children[0]
-      right = String.filter Char.IsLetter children[1] }
+      left = String.filter Char.IsLetterOrDigit children[0]
+      right = String.filter Char.IsLetterOrDigit children[1] }
 
 let makeGraph (nodes: Node seq) : Map<string, Node> =
     nodes |> Seq.fold (fun map node -> Map.add node.label node map) Map.empty
@@ -59,3 +59,34 @@ let input = File.ReadAllLines "input.txt"
 
 let dirs, graph = parseInput input
 run graph (List.ofSeq dirs) "AAA" |> printfn "Part 1: %d"
+
+let runParallel graph dirs nodeLabels =
+    let rec run' ds nodeLabels count =
+        match ds with
+        | [] -> run' dirs nodeLabels count
+        | h :: t ->
+            if Array.forall (fun (l: string) -> l.EndsWith('Z')) nodeLabels then
+                count
+            else
+                let nextLabels =
+                    Array.map
+                        (fun label ->
+                            let { left = left; right = right } =
+                                Map.find label graph
+
+                            match h with
+                            | L -> left
+                            | R -> right)
+                        nodeLabels
+
+                run' t nextLabels (count + 1)
+
+    run' dirs nodeLabels 0
+
+let startLabels =
+    graph
+    |> Map.keys
+    |> Seq.filter (fun (l: string) -> l.EndsWith('A'))
+    |> Seq.toArray
+
+runParallel graph (List.ofSeq dirs) startLabels |> printfn "Part 2: %d"
