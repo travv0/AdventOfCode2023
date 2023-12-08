d8d488d6643be857183bca804c80390774f84e68
Travis <tssund93@gmail.com>
2023-12-08 15:40:43 -0600

    Add solution file for day 08 challenge
    This commit introduces a new solution file for the day 08 challenge. The code deals with parsing input data and building a graph structure based on it. A "run" function is also implemented to traverse the graph according to specific rules.

---
+++ day08/day08.fsx
@@ -0,0 +1,61 @@
+﻿open System
+open System.IO
+
+type Dir =
+    | L
+    | R
+
+type Node =
+    { label: string
+      left: string
+      right: string }
+
+let parseNode (s: string) =
+    let parts = s.Split(" = ")
+    let children = parts[1].Split(", ")
+
+    { label = parts[0]
+      left = String.filter Char.IsLetter children[0]
+      right = String.filter Char.IsLetter children[1] }
+
+let makeGraph (nodes: Node seq) : Map<string, Node> =
+    nodes |> Seq.fold (fun map node -> Map.add node.label node map) Map.empty
+
+let parseInput (lines: string array) =
+    let dirLine = lines[0]
+
+    let dirs =
+        Seq.map
+            (fun d ->
+                match d with
+                | 'L' -> L
+                | 'R' -> R
+                | _ -> failwith "invalid input")
+            dirLine
+
+    let nodes = lines[2..] |> Seq.map parseNode
+    dirs, makeGraph nodes
+
+let run graph dirs nodeLabel =
+    let rec run' ds nodeLabel count =
+        match ds with
+        | [] -> run' dirs nodeLabel count
+        | h :: t ->
+            if nodeLabel = "ZZZ" then
+                count
+            else
+                let { left = left; right = right } = Map.find nodeLabel graph
+
+                run'
+                    t
+                    (match h with
+                     | L -> left
+                     | R -> right)
+                    (count + 1)
+
+    run' dirs nodeLabel 0
+
+let input = File.ReadAllLines "input.txt"
+
+let dirs, graph = parseInput input
+run graph (List.ofSeq dirs) "AAA" |> printfn "Part 1: %d"
